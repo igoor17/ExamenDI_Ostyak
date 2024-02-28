9b958308371a309d52e53072d1849b2f4546fd01
igoor17 <131800874+igoor17@users.noreply.github.com>
2024-02-28 16:03:12 +0100

    Delete GestionBiblioteca directory

--- GestionBiblioteca/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.gestionbiblioteca {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.gestionbiblioteca to javafx.fxml;
-    exports com.example.gestionbiblioteca;
-}
